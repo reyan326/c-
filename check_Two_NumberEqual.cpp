2de0b2bacae81e5ecadbc5d877f3d8321ec96899
reyan326 <58131099+reyan326@users.noreply.github.com>
2021-10-12 12:07:46 +0530

    Create check_Two_NumberEqual.cpp

---
+++ check_Two_NumberEqual.cpp
@@ -0,0 +1,24 @@
+/*  Write a  program to accept two integers and check whether they are equal or not */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   int a, b;
+   cout<<"Enter value of Number1 and  Number2  = ",cin>>a>>b;
+   if(a==b)
+   {
+       cout<<"Number1 and Number2 are equal";
+   }
+   else
+   {
+       cout<<"Number1 and Number2 are not equal";
+   }
+   
+   
+
+    return 0;
+}

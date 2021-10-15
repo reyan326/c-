c53781fca6b387093adc509d9a90ac1ce0d9488c
reyan326 <58131099+reyan326@users.noreply.github.com>
2021-10-15 15:08:41 +0530

    Create natural_number.cpp

---
+++ natural_number.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Write a program in C++ to find the first 10 natural numbers
+
+*******************************************************************************/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+   int i;
+   for(i = 1; i<=10; i++)
+   {
+       cout<<i<<endl;
+   }
+   
+   
+    return 0;
+}
